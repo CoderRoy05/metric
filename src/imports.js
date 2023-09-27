9fb96d9e383fa0b4cbd4a9b72ba7baa6f89a0fb7
CoderRoy05 <royalalawode@gmail.com>
2023-09-27 17:42:12 +0100

    removed bug

--- src/imports.js
+++
@@ -1,9 +0,0 @@
-import metric_one from './assets/metric_one.png';
-import metric_two from './assets/metric_two.png';
-import metric_three from './assets/metric_three.png';
-
-export {
-    metric_one,
-    metric_two,
-    metric_three,
-};
